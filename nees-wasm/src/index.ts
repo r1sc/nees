7f6c210c5a8154da192123d6a52eb033c9289558
r1sc <629669+r1sc@users.noreply.github.com>
2024-08-18 13:34:29 +0200

    Simplify build + add gamepad support

--- nees-wasm/src/index.ts
+++ nees-wasm/src/index.ts
@@ -2,14 +2,25 @@ import { make_renderer } from "./renderer";
 import wasminit, { get_framebuffer_ptr, init, tick } from "../pkg/nees_wasm";
 import wasm_path from "../pkg/nees_wasm_bg.wasm";
 
+// Get rom path from query string
+const rom_path = new URLSearchParams(window.location.search).get("rom");
+if (rom_path === null) {
+    alert("No rom path provided in query string");
+    throw new Error("No rom path provided in query string");
+}
+
 declare global {
     interface Window {
         waveout_callback: (sample: number) => void;
     }
 }
 
-window.onclick = async function () {
-    window.onclick = null;
+const start_btn = document.createElement("button");
+start_btn.textContent = "Click to start";
+document.body.appendChild(start_btn);
+
+start_btn.onclick = async () => {
+    start_btn.remove();
 
     const audio = new AudioContext({ sampleRate: 15720 });
     await audio.audioWorklet.addModule("nes-audio-processor.js");
@@ -23,8 +34,12 @@ window.onclick = async function () {
         audioNode.port.postMessage({ sample: sample });
     };
 
+    async function loadwasm(){
+        const response = fetch(wasm_path as unknown as string);
+        return await wasminit(response);        
+    }
 
-    const o = await wasminit(wasm_path);
+    const o = await loadwasm();
 
     const { gl, draw } = make_renderer();
 
@@ -34,7 +49,7 @@ window.onclick = async function () {
         return new Uint8Array(buffer);
     }
 
-    const rom = await get_rom_from_url("roms/smb3.nes");
+    const rom = await get_rom_from_url(rom_path);
 
     const nees_ptr = init(rom);
 
@@ -48,63 +63,95 @@ window.onclick = async function () {
     let need_render = true;
     const target_ms = 1000 / 60;
 
-    let player1_buttons_down = 0;
-    let player2_buttons_down = 0;
+    let player_buttons: [number, number] = [0, 0];
 
     window.addEventListener("keydown", (e) => {
         if (e.key === "s") {
-            player1_buttons_down |= 1 << 0;
+            player_buttons[0] |= 1 << 0;
         } else if (e.key === "a") {
-            player1_buttons_down |= 1 << 1;
+            player_buttons[0] |= 1 << 1;
         } else if (e.key === "q") {
-            player1_buttons_down |= 1 << 2;
+            player_buttons[0] |= 1 << 2;
         } else if (e.key === "w") {
-            player1_buttons_down |= 1 << 3;
+            player_buttons[0] |= 1 << 3;
         } else if (e.key === "ArrowUp") {
-            player1_buttons_down |= 1 << 4;
+            player_buttons[0] |= 1 << 4;
         } else if (e.key === "ArrowDown") {
-            player1_buttons_down |= 1 << 5;
+            player_buttons[0] |= 1 << 5;
         } else if (e.key === "ArrowLeft") {
-            player1_buttons_down |= 1 << 6;
+            player_buttons[0] |= 1 << 6;
         } else if (e.key === "ArrowRight") {
-            player1_buttons_down |= 1 << 7;
+            player_buttons[0] |= 1 << 7;
         }
 
     });
 
     window.addEventListener("keyup", (e) => {
         if (e.key === "s") {
-            player1_buttons_down &= ~(1 << 0);
+            player_buttons[0] &= ~(1 << 0);
         } else if (e.key === "a") {
-            player1_buttons_down &= ~(1 << 1);
+            player_buttons[0] &= ~(1 << 1);
         } else if (e.key === "q") {
-            player1_buttons_down &= ~(1 << 2);
+            player_buttons[0] &= ~(1 << 2);
         } else if (e.key === "w") {
-            player1_buttons_down &= ~(1 << 3);
+            player_buttons[0] &= ~(1 << 3);
         } else if (e.key === "ArrowUp") {
-            player1_buttons_down &= ~(1 << 4);
+            player_buttons[0] &= ~(1 << 4);
         } else if (e.key === "ArrowDown") {
-            player1_buttons_down &= ~(1 << 5);
+            player_buttons[0] &= ~(1 << 5);
         } else if (e.key === "ArrowLeft") {
-            player1_buttons_down &= ~(1 << 6);
+            player_buttons[0] &= ~(1 << 6);
         } else if (e.key === "ArrowRight") {
-            player1_buttons_down &= ~(1 << 7);
+            player_buttons[0] &= ~(1 << 7);
         }
     });
 
+
     (function render() {
+
         const now = performance.now();
         let delta = now - last_time;
         last_time = now;
 
-        if(delta > 500) {
+        if (delta > 500) {
             delta = 500;
         }
 
         accum += delta;
 
+        const [gp1, gp2] = navigator.getGamepads();
+        function set_buttons_down(gp: Gamepad, player_index: number) {
+            player_buttons[player_index] = 0;
+            if (gp.buttons[0].pressed) {
+                player_buttons[player_index] |= 1 << 0;
+            }
+            if (gp.buttons[2].pressed) {
+                player_buttons[player_index] |= 1 << 1;
+            }
+            if (gp.buttons[8].pressed) {
+                player_buttons[player_index] |= 1 << 2;
+            }
+            if (gp.buttons[9].pressed) {
+                player_buttons[player_index] |= 1 << 3;
+            }
+            if (gp.axes[1] < -0.5) {
+                player_buttons[player_index] |= 1 << 4;
+            }
+            if (gp.axes[1] > 0.5) {
+                player_buttons[player_index] |= 1 << 5;
+            }
+            if (gp.axes[0] < -0.5) {
+                player_buttons[player_index] |= 1 << 6;
+            }
+            if (gp.axes[0] > 0.5) {
+                player_buttons[player_index] |= 1 << 7;
+            }
+        }
+        if (gp1) set_buttons_down(gp1, 0);
+        if (gp2) set_buttons_down(gp2, 1);
+
         while (accum >= target_ms) {
-            tick(nees_ptr, fb_ptr, player1_buttons_down, player2_buttons_down);
+            tick(nees_ptr, fb_ptr, player_buttons[0], player_buttons[1]);
             accum -= target_ms;
             need_render = true;
         }
