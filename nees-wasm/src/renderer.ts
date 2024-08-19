c5af810fafb7f0f340c9abbab0adbbd95342888b
r1sc <629669+r1sc@users.noreply.github.com>
2024-08-20 01:19:55 +0200

    Use same shader for ts-project

--- nees-wasm/src/renderer.ts
+++ nees-wasm/src/renderer.ts
@@ -1,4 +1,6 @@
 
+import shader_src from "../../shaders/crt.glsl";
+
 function create_shader(gl: WebGL2RenderingContext, program_src: string): WebGLProgram {
     function load_shader(src: string, type: "vertex" | "fragment") {
         const shader = gl.createShader(type === "vertex" ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER);
@@ -61,48 +63,6 @@ export function make_renderer() {
 
     const glsl = (x: any) => x;
 
-    const shader_src = glsl`
-    #ifdef VS
-
-layout(location = 0) in vec2 a_pos;
-layout(location = 1) in vec2 a_uv;
-
-out vec2 uv;
-    
-void main() {
-    uv = a_uv;
-    gl_Position = vec4(a_pos, 0.0, 1.0);
-}    
-#endif
-
-#ifdef FS
-
-precision highp float;
-    
-float u_bending_factor = 0.4;
-float u_darkness = 0.5;
-float u_num_lines = 240.0;
-float u_horizontal_offset = 0.0;
-uniform sampler2D u_image;
-in vec2 uv;
-
-out vec4 outColor;
-
-
-void main() {
-    vec2 nuv = uv - vec2(0.5, 0.5);
-    vec2 offset = vec2(nuv.x * abs(nuv.x) * nuv.y * nuv.y * u_bending_factor, nuv.x * nuv.x * nuv.y * abs(nuv.y) * u_bending_factor);
-    vec2 uvo = uv + offset;
-        
-    float tint = mix(1.0, u_darkness, smoothstep(0.0, 0.5, fract(uvo.y * u_num_lines)) - smoothstep(0.5, 1.0, fract(uvo.y * u_num_lines)));
-	outColor = vec4(texture(u_image, uvo + vec2(u_horizontal_offset, 0.0)).bgr * tint, 1);
-
-    if(uvo.x < u_horizontal_offset || uvo.y < 0.0 || uvo.x >= (1.0 - u_horizontal_offset) || uvo.y >= 1.0) {
-		outColor = vec4(0.0, 0.0, 0.0, 1.0);
-	}
-}    
-#endif
-    `;
 
     const program = create_shader(gl, shader_src);
     gl.useProgram(program);
--- shaders/crt.glsl
+++ shaders/crt.glsl
@@ -15,9 +15,10 @@ void main() {
 
 precision highp float;
     
-float u_bending_factor = 0.5;
+float u_bending_factor = 0.4;
 float u_darkness = 0.5;
 float u_num_lines = 240.0;
+float u_horizontal_offset = 0.0;
 uniform sampler2D u_image;
 in vec2 uv;
 
@@ -26,15 +27,13 @@ out vec4 outColor;
 
 void main() {
     vec2 nuv = uv - vec2(0.5, 0.5);
-    float x = nuv.x * nuv.x;
-    float y = nuv.y * abs(nuv.y);
-    vec2 offset = vec2(0, x * y * u_bending_factor);
+    vec2 offset = vec2(nuv.x * abs(nuv.x) * nuv.y * nuv.y * u_bending_factor, nuv.x * nuv.x * nuv.y * abs(nuv.y) * u_bending_factor);
     vec2 uvo = uv + offset;
         
     float tint = mix(1.0, u_darkness, smoothstep(0.0, 0.5, fract(uvo.y * u_num_lines)) - smoothstep(0.5, 1.0, fract(uvo.y * u_num_lines)));
-	outColor = vec4(texture(u_image, uvo).rgb * tint, 1);
+	outColor = vec4(texture(u_image, uvo + vec2(u_horizontal_offset, 0.0)).bgr * tint, 1);
 
-    if(uvo.x < 0.0 || uvo.y < 0.0 || uvo.x >= 1.0 || uvo.y >= 1.0) {
+    if(uvo.x < u_horizontal_offset || uvo.y < 0.0 || uvo.x >= (1.0 - u_horizontal_offset) || uvo.y >= 1.0) {
 		outColor = vec4(0.0, 0.0, 0.0, 1.0);
 	}
 }    
